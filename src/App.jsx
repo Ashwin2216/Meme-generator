06c8bb17f056d57c8a4dd1c2c70170f5a5f3007c
Ashwin2216 <ashwinchandran76@gmail.com>
2024-01-23 19:51:25 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./assets/Header";
-import Meme from "./assets/Meme";
 import { useEffect, useState } from "react";
 function App() {
   const [meme, setMeme] = useState({
@@ -22,10 +21,10 @@ function App() {
     const randomMemeimg = allMeme[randomNumber].url;
 
     setMeme((prevMeme) => {
-      ({
+      return {
         ...prevMeme,
         randomImg: randomMemeimg,
-      });
+      };
     });
     console.log(meme, "checking state of meme");
   };
@@ -47,12 +46,34 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <Meme
-        memeData={meme}
-        handleChange={handleChange}
-        getMemeImage={getMemeImage}
-        handleSubmit={handleSubmit}
-      />
+      <section className="input-section">
+        <form onSubmit={handleSubmit} className="form " action="">
+          <input
+            placeholder="Top-text"
+            className="text-box-1"
+            type="text"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
+          />
+          <input
+            placeholder="Bottom-text"
+            className="text-box-2"
+            type="text"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
+          />
+          <button className="meme-button" type="button" onClick={getMemeImage}>
+            New meme image
+          </button>
+        </form>
+        <div className="image-container ">
+          <img src={meme.randomImg} alt="meme-image" />
+          <h2 className="meme-text meme-text-top">{meme.topText}</h2>
+          <h2 className="meme-text meme-text-bottom ">{meme.bottomText}</h2>
+        </div>
+      </section>
     </div>
   );
 }
